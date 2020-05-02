d47135f2f9ed495fda2df380d884da5c5c4b373a
Max <mx.a.kovalenko@gmail.com>
2020-05-02 22:07:13 +0300

    Reforms
    Test version of prime number calculator

---
+++ prime_or_not.py
@@ -0,0 +1,19 @@
+print("Hi!")
+print("It's prime number calculator")
+while True:
+    a = int(input("Enter number to know, is it prime or not: "))
+    i = 2
+    j = 0
+    while i * i <= a and j != 1:
+        if a % i == 0:
+            j = 1
+            i = i + 1
+        elif a % i == 0:
+            i = i + 1
+        if j == 1:
+            print("This number is not prime")
+            break
+        elif j != 1:
+            print("This number is prime")
+            break
+    
