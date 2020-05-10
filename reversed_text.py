5706486636444f3d05b3cd25286009a106bd4c45
Max <mx.a.kovalenko@gmail.com>
2020-05-10 17:30:35 +0300

    Reforms

--- reversed_text.py
+++ reversed_text.py
@@ -9,5 +9,5 @@ while True:
     # Вводим переменную для текста наоборот
     reversed_text = text[length::-1]
     # Выводим текст и текст наоборот
-    print("The reversed text of " + text, "is:",reversed_text)
+    print("The reversed text of " + text, "is:", reversed_text)
     # Перевод: Код символа",symbol,"— это: ",code
